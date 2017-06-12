63bfcaa33aaee7d0abdb92145c42cd5adc96df6d
liugangyi <liugangyi@allinmd.cn>
2017-06-12 15:51:21 +0800

    终结

---
+++ DrawDemoTest/PracticeEight/PracticeEightView.h
@@ -0,0 +1,15 @@
+//
+//  PracticeEightView.h
+//  DrawDemoTest
+//
+//  Created by liugangyi on 2017/5/19.
+//  Copyright © 2017年 com.allinmd.cn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PracticeEightView : UIView
+
+
+
+@end
---
+++ DrawDemoTest/PracticeEight/PracticeEightView.m
@@ -0,0 +1,22 @@
+//
+//  PracticeEightView.m
+//  DrawDemoTest
+//
+//  Created by liugangyi on 2017/5/19.
+//  Copyright © 2017年 com.allinmd.cn. All rights reserved.
+//
+
+#import "PracticeEightView.h"
+
+@implementation PracticeEightView
+
+
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+    
+
+    
+}
+
+
+@end
